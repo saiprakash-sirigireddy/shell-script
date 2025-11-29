cc842a484d742debc5440379b94338f33bc2aae5
saiprakash-sirigireddy <saiprakash.sirigireddy@gmail.com>
2025-11-29 23:18:55 +0530

    added special variables file

---
+++ 07-special-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# To get all variables passed - $@
+# To get number of variables - $#
+# Script name - $0
+# Present working directory - $PWD
+# Home directory of current user - $HOME
+# Which user is running the script - $USER
+# Process id of current script - $$
+# Process id of last command in background - $!
+
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running the script: $USER"
+echo "Process id of current script: $$"
+sleep 60 &
+echo "Process id of last command in background: $!"
