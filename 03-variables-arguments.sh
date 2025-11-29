4b3b712368d36f16cb33df04af0641c9012a0d27
saiprakash-sirigireddy <saiprakash.sirigireddy@gmail.com>
2025-11-29 20:11:20 +0530

    added new file variables creds

--- 03-variables-arguments.sh
+++ 03-variables-arguments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PERSON1=$1 # $1 represents first argument provide as input while executing
-PERSON2=$2
+PERSON2=$2 # arguments are runtime inputs
 echo "$PERSON1:: Hi $PERSON2"
 echo "$PERSON2:: Hi $PERSON1"
 echo "$PERSON1:: How are you?"
