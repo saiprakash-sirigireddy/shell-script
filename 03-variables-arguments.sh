9d95a02d51ff3e206524be53d2d8c6eb38940bd0
saiprakash-sirigireddy <saiprakash.sirigireddy@gmail.com>
2025-11-29 20:06:51 +0530

    added new file variables arguments

--- 03-variables-arguments.sh
+++ 03-variables-arguments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Krishna # no space before and after equal and variable should be all caps
-PERSON2=Arjuna
+PERSON1=$1 # $1 represents first argument provide as input while executing
+PERSON2=$2
 echo "$PERSON1:: Hi $PERSON2"
 echo "$PERSON2:: Hi $PERSON1"
 echo "$PERSON1:: How are you?"
