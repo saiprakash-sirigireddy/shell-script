9f5c0ddb81a1208cf666c7302f9359f2ca9084b7
saiprakash-sirigireddy <saiprakash.sirigireddy@gmail.com>
2025-11-30 19:39:32 +0530

    new logs file added

---
+++ 12-logs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# notations in redirectors 
+
+# < -- used for input 
+# 
+# > -- used for output
+# Ex: ls -l > output.log  =>  ls -l command output will stored to output.log
+# 1 -- denotes success
+# 2 -- denotes failure
+# & -- denotes both success and failure
+
+#!/bin/bash
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." f1)
+TIMESTAMP=$(+%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ] # taking last command status as first argument
+    then
+        echo -e "$2 ... $R FAILURE"  # taking string in second argument
+    else
+        echo -e "$2 ... $G SUCCESS" 
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>LOG_FILE_NAME
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: You must have sudo access to execute this script" &>>LOG_FILE_NAME
+    exit 1
+fi
+
+INSTALLED(){
+    dnf list installed $1 &>>LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $1 -y &>>LOG_FILE_NAME
+        VALIDATE $? "Installing $1"
+    else
+        echo -e "$1 is already ... $Y INSTALLED" 
+    fi
+}
+
+SOFTWARES=("mysql" "git")
+INSTALLED ${SOFTWARES[0]}
+INSTALLED ${SOFTWARES[1]}
