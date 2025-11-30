9c30a1f649d91b8f9df3a2bc0e0688346dc3e0a1
saiprakash-sirigireddy <saiprakash.sirigireddy@gmail.com>
2025-11-30 19:56:20 +0530

    new logs file modified

--- 12-logs.sh
+++ 12-logs.sh
@@ -12,7 +12,7 @@
 
 #!/bin/bash
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." f1)
+LOG_FILE=$(echo $0 | awk -F'.' '{print $1}')
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 R="\e[31m"
