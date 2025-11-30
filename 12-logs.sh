46ede9ce85ce9d43ffc18d232c666a59230351dd
saiprakash-sirigireddy <saiprakash.sirigireddy@gmail.com>
2025-11-30 22:19:29 +0530

    new logs file modified

--- 12-logs.sh
+++ 12-logs.sh
@@ -18,35 +18,36 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 echo $LOG_FILE
 echo $LOG_FILE_NAME
 echo $TIMESTAMP
 VALIDATE(){
     if [ $1 -ne 0 ] # taking last command status as first argument
     then
-        echo -e "$2 ... $R FAILURE"  # taking string in second argument
+        echo -e "$2 ... $R FAILURE $N"  # taking string in second argument
     else
-        echo -e "$2 ... $G SUCCESS" 
+        echo -e "$2 ... $G SUCCESS $N" 
     fi
 }
 
-echo "Script started executing at: $TIMESTAMP" &>>LOG_FILE_NAME
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: You must have sudo access to execute this script" &>>LOG_FILE_NAME
+    echo -e "$R ERROR:: You must have sudo access to execute this script $N" &>>$LOG_FILE_NAME
     exit 1
 fi
 
 INSTALLED(){
-    dnf list installed $1 &>>LOG_FILE_NAME
+    dnf list installed $1 &>>$LOG_FILE_NAME
     if [ $? -ne 0 ]
     then
-        dnf install $1 -y &>>LOG_FILE_NAME
+        dnf install $1 -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing $1"
     else
-        echo -e "$1 is already ... $Y INSTALLED" 
+        echo -e "$1 is already ... $Y INSTALLED $N" 
     fi
 }
 
